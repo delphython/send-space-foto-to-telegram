ea2c8c458fbf77612c68b229d24d8b153a6ff027
delphython <delphython@gmail.com>
2021-08-21 22:04:09 +0300

    Add start number to enumerate

--- main.py
+++ main.py
@@ -67,12 +67,12 @@ def fetch_nasa_epic_photos(token):
     response.raise_for_status()
     epic_images = response.json()
 
-    for number, epic_image in enumerate(epic_images):
+    for number, epic_image in enumerate(epic_images, start=1):
         image_name = epic_image[image_header]
         image_dt = datetime.strptime(epic_image["date"], "%Y-%m-%d %H:%M:%S")
         image_url = "https://api.nasa.gov/EPIC/archive/natural/%04d/%02d/%02d/png/%s.png?api_key=%s" % (
             image_dt.year, image_dt.month, image_dt.day, image_name, token)
-        image_file_name = f"epic{number + 1}.png"
+        image_file_name = f"epic{number}.png"
         download_image(image_url, image_file_name)
 
 
